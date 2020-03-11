b7796c35cbd76f1254b01b720aac634e9da79090
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-11 16:54:49 +0530

    REFACTORED: Condition to compute part time employee wage

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -5,15 +5,18 @@ public class EmployeeWageComputation {
         System.out.println("/**************************/ WELCOME TO EMPLOYEE WAGE COMPUTATION /**************************/");
         //CONSTANTS
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         //VARIABLES
         int empHrs = 0;
         int empWage = 0;
         //GENERATING RANDOM VALUE
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         //COMPUTING EMPLOYEE WAGE
         if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
+        } else if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
         } else {
             empHrs = 0;
         }
