e6fd3520d52552c986948db72e0ba0c77781cb11
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-11 16:48:23 +0530

    ADDED: Condition to calculate daily employee wage

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -3,14 +3,21 @@ package com.bridgelabz.employeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO EMPLOYEE WAGE COMPUTATION /**************************/");
-
+        //CONSTANTS
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        //VARIABLES
+        int empHrs = 0;
+        int empWage = 0;
+        //GENERATING RANDOM VALUE
         double empCheck = Math.floor(Math.random() * 10) % 2;
-
+        //COMPUTING EMPLOYEE WAGE
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
+            empHrs = 8;
         } else {
-            System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage :" + empWage);
     }
 }
