249e473a9184a6eaf57c0366f939c9c9ed51a064
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-11 17:27:19 +0530

    ADDED: Loop for calculating wage for month

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -5,28 +5,34 @@ public class EmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUMBER_OF_WORKING_DAYS = 20;
 
     //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO EMPLOYEE WAGE COMPUTATION /**************************/");
 
         //VARIABLES
-        int empHrs = 0;
-        int empWage = 0;
-        //GENERATING RANDOM VALUE
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+
         //COMPUTING EMPLOYEE WAGE
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        for (int day = 1; day <= NUMBER_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Employee Wage for day "+day+ " is :" + empWage);
+            totalEmpWage += empWage;
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage :" + empWage);
+        //DISPLAYING TOTAL EMPLOYEE WAGE FOR A MONTH
+        System.out.println("Total Employee Wage for a month is :" + totalEmpWage);
     }
 }
