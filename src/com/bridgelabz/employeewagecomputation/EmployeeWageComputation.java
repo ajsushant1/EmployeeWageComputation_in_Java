430962b43826433e3b6dc7ab3782a16d6c51a270
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-11 17:08:54 +0530

    ADDED: Switch case statement

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -1,24 +1,30 @@
 package com.bridgelabz.employeewagecomputation;
 
 public class EmployeeWageComputation {
+    //CONSTANTS
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
+    //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO EMPLOYEE WAGE COMPUTATION /**************************/");
-        //CONSTANTS
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+
         //VARIABLES
         int empHrs = 0;
         int empWage = 0;
         //GENERATING RANDOM VALUE
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         //COMPUTING EMPLOYEE WAGE
-        if (empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-        } else if (empCheck == IS_PART_TIME) {
-            empHrs = 4;
-        } else {
-            empHrs = 0;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage :" + empWage);
