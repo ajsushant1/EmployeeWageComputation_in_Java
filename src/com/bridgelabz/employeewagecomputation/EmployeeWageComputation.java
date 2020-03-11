c694e52fb3d263477a2a32ce7e89fda60002c661
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-11 16:10:47 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeewagecomputation;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("/**************************/ WELCOME TO EMPLOYEE WAGE COMPUTATION /**************************/");
+    }
+}
